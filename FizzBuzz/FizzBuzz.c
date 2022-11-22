ed06269497a18701cc54d969357c386ef299e089
Hasan Kemal ECE <109526257+ecehasankemal@users.noreply.github.com>
2022-11-22 04:20:00 +0300

    Add files via upload

--- FizzBuzz/FizzBuzz.c
+++ FizzBuzz/FizzBuzz.c
@@ -1,10 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 char	**fizzBuzz(int n, int *returnSize)
 {
-	int	iter;
-	int	dop;
+	char	**return_var;
+	int	count;
 
-	iter = 0;
-	dop = 1;
+	count = 0;
+	return_var = (char **)(malloc(sizeof(char *) * n));
+	while (++count <= n)
+	{
+		if ((count % 3 == 0) && (count % 5 == 0))
+			return_var[count - 1] = "FizzBuzz";
+		else if (count % 3 == 0)
+			return_var[count - 1] = "Fizz";
+		else if (count % 5 == 0)
+			return_var[count - 1] = "Buzz";
+		else
+		{
+			return_var[count - 1] = (char *)(malloc(sizeof(char) * 10));
+			sprintf((char *)return_var[count - 1], "%d", count);
+		}
+	}
 	*returnSize = n;
-	char **result = (char **)malloc(n * sizeof(char *));
-	while
+	return (return_var);
+}
+
+int    main(void)
+{
+    char	**str;
+    int		in;
+    int		*tmp;
+
+    *tmp = 0;
+    printf("enter input: ");
+    scanf("%d",&in);
+    str = fizzBuzz(in,tmp);
+    for(int i = 0; i < in ; i++)
+    {
+        printf("%s\n",str[i]);
+    }
+    free(str);
+    return (0);
+}
